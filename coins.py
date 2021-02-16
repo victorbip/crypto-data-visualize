76c067c70fa7e12a13141cfb849382c3d2cbcf0a
Victor Brinkhorst <victorbrnk@gmail.com>
2021-02-16 02:20:53 +0100

    Added some code comments

--- coins.py
+++ coins.py
@@ -8,21 +8,22 @@ def raise_type_error(message):
 
 
 class CoinClient:
-
     def __init__(self, ticker):
         self.ticker = ticker
         self.cg = CoinGeckoAPI()
         self.response_dict = dict()
 
+    # Gets raw data between start and end date (UNIX time format)
     def get_between(self, start_date: float, end_date: float):
+        # Returns one CoinGecko response object for coin specified
         if isinstance(self.ticker, str):
             return self.cg.get_coin_market_chart_range_by_id(self.ticker, config.currency, start_date, end_date)
-
+        # Returns a dict of CoinGecko response objects for coins specified
         elif isinstance(self.ticker, list):
             for c in self.ticker:
                 self.response_dict[c] = self.cg.get_coin_market_chart_range_by_id(c, config.currency, start_date, end_date)
             return self.response_dict
-
+        # Raise TypeError is self.ticker isn't a string or list.
         else:
             raise_type_error("Ticker must either be a string or list of strings.")
 
