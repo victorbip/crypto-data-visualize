2063866032534ca9690f13f69d4f67bb68ea6f67
Victor Brinkhorst <victorbrnk@gmail.com>
2021-02-16 13:24:23 +0100

    Fixed beautifying UNIX timestamp to Datetime format. Supports beautifying multiple coins at a time.

--- coins.py
+++ coins.py
@@ -4,11 +4,12 @@ from pycoingecko import CoinGeckoAPI
 
 
 def raise_type_error(message):
+    # Raises a type error
     raise TypeError(message)
 
 
 class CoinClient:
-    # Response object keys
+    # Response object keys (per coin)
     # prices
     # market_caps
     # total_volumes
@@ -22,30 +23,44 @@ class CoinClient:
     def get_between(self, start_date: float, end_date: float):
         # Returns one CoinGecko response object for coin specified
         if isinstance(self.ticker, str):
-            return self.cg.get_coin_market_chart_range_by_id(self.ticker, config.currency, start_date, end_date)
+            return False, self.cg.get_coin_market_chart_range_by_id(self.ticker, config.currency, start_date, end_date)
+
         # Returns a dict of CoinGecko response objects for coins specified
         elif isinstance(self.ticker, list):
             for c in self.ticker:
-                self.response_dict[c] = self.cg.get_coin_market_chart_range_by_id(c, config.currency, start_date, end_date)
-            return self.response_dict
+                self.response_dict[c] = self.cg.get_coin_market_chart_range_by_id(c, config.currency, start_date,
+                                                                                  end_date)
+
+            return True, self.response_dict
         # Raise TypeError is self.ticker isn't a string or list.
         else:
             raise_type_error("Ticker must either be a string or list of strings.")
 
 
 class RawResponse:
+    # Turns CoinClient response object into readable (UNIX -> DATETIME)
     def __init__(self, data):
         self.data = data
+        self.response_dict = dict()
 
-    def beautify(self):
-        if isinstance(self.data, dict):
-            pass
+    def iterate(self, ls):
+        arr = list()
+        for i in ls:
+            arr.append([datetime.fromtimestamp(i[0] / 1000).date(), i[1]])
+        return arr
 
-        elif isinstance(self.data, list):
-            for c in self.data:
-                pass
-
+    def beautify_between(self):
+        l, data = self.data[0], self.data[1]
+        if isinstance(data, dict):
+            # Check if data is a list or not, if yes it iterates over the names of the coins
+            if l:
+                for c in data:
+                    self.response_dict[c] = {}
+                    for k in data[c]:
+                        self.response_dict[c].update({k: self.iterate(data[c][k])})
+            else:
+                for k in data:
+                    self.response_dict.update({k: self.iterate(data[k])})
         else:
-            raise_type_error("Data must either be a dict or a list of dicts")
-
-# datetime.fromtimestamp(end_date)
+            raise_type_error("Data must either be of type dict")
+        return self.response_dict
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from datetime import datetime, timedelta
-import coins as coin
+import coins
 
 end_date = datetime.today().timestamp()
 start_date = (datetime.today() - timedelta(days=365)).timestamp()
-coin = coin.CoinClient('bitcoin')
+
+coin = coins.CoinClient('stellar')
 resp = coin.get_between(start_date, end_date)
 
-print(resp)
 
-for c in resp['prices']:
-    print(c)
+raw = coins.RawResponse(resp)
+d = raw.beautify_between()
+print(d)
