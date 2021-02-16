9f65c11d23c354f855755ca60670c3942ccd829a
Victor Brinkhorst <victorbrnk@gmail.com>
2021-02-16 15:43:19 +0100

    Made a working plotting function (line_graph_price). Needs to be beautified a bit so it looks nicer and need to add support for multiple coins.

--- plot.py
+++ plot.py
@@ -1,12 +1,18 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+import seaborn as sns
+sns.set()
 
 
 def sanitize_response(obj, key, multiple: bool):
+    # Get an X and Y axis for the key (prices, total_volumes, market_caps) for coins.
+    # Multiple flag is used to see if there's multiple coins given
+
+    # Creating response dictionary and x and y list
     response = dict()
     x = list()
     y = list()
+
     if multiple:
         for c in obj:
             response[c] = {}
@@ -25,11 +31,20 @@ def sanitize_response(obj, key, multiple: bool):
 
 
 class PlotArray:
+    # Plot a graph from an array/list (x and y axis are lists)
 
     def __init__(self, x: list, y: list):
         self.x_axis = x
         self.y_axis = y
 
-
-    def plot_graph(self):
-        pass
+    def line_graph_price(self, coin: str, multiple: bool):
+        # Plot a line graph for the price overtime
+        if multiple:
+            pass
+        else:
+            plt.plot(self.x_axis, self.y_axis)
+            plt.xlabel('Timeframe (YYYY-MM)')
+            plt.ylabel(f'{coin} price')
+            plt.title(f'Coin price graph')
+            plt.legend([coin])
+        plt.show()
